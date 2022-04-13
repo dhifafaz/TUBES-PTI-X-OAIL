9f06057b985d4be73471b890e024723c58c4038a
Muhammad Farhan <farhanbe123@gmail.com>
2022-04-14 02:04:59 +0700

    profil petugas clear

--- Petugas/src/style/profilStyle.js
+++ Petugas/src/style/profilStyle.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+
 const styles = StyleSheet.create({
     color: {
         backgroundColor: '#151D3B',
@@ -12,16 +13,66 @@ const styles = StyleSheet.create({
         fontFamily: 'Ubuntu-Bold',
         color: '#ECECEC',
     },
-    enter40: {
+    imageProfile: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        marginVertical: 20,
+    },
+    image: {
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+    },
+    card: {
+        marginBottom: 20,
+    },
+    title: {
+        fontFamily: 'Ubuntu-Bold',
+        fontSize: 18,
+        color: '#ECECEC',
+        marginBottom: 5,
+    },
+    isiText: {
+        fontFamily: 'Ubuntu-Light',
+        fontSize: 16,
+        backgroundColor: '#ECECEC',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 2,
+    },
+    button: {
+        backgroundColor: '#ECECEC',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 2,
+    },
+    button2: {
+        backgroundColor: '#ECECEC',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 2,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: 'center',
+    },
+    theText: {
+        fontFamily: 'Ubuntu-Light',
+        fontSize: 16,
+    },
+    buttonOut:{
         height: 40,
+        backgroundColor: '#FF0000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
     },
-    enter30: {
-        height: 30,
+    outText: {
+        fontFamily: 'Ubuntu-Bold',
+        fontSize: 18,
+        color: '#ECECEC',
     },
-    enter20: {
-        height: 20,
-    },
-
+    
 })
 
 export default styles;
