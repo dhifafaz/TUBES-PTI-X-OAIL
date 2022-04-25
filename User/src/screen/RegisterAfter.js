73779c0965dc596a05aadb7ce97c7ae6a4a1a968
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-04-25 21:31:06 +0700

    nyoba nyoba masuk registrasi

---
+++ User/src/screen/RegisterAfter.js
@@ -0,0 +1,296 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Text,
+    SafeAreaView,
+    View,
+    TextInput,
+    TouchableOpacity,
+    ScrollView,
+    Alert,
+} from 'react-native';
+import styles from '../style/LoginPageStyles';
+import { Image, } from 'react-native-elements';
+import { launchImageLibrary } from 'react-native-image-picker';
+import { useRoute } from '@react-navigation/native';
+import storage, { firebase } from '@react-native-firebase/storage';
+
+
+const RegisterAfter = ({ navigation }) => {
+
+
+
+    const route = useRoute().params
+
+    const [imagePicker, setImagePicker] = useState(null);
+    const [imagePicker2, setImagePicker2] = useState(null);
+
+    const [readyUploadKTP, setUploadKTP] = useState(null);
+    const [readyUploadPhoto, setUploadPhoto] = useState(null);
+
+    const [uploading, setUploading] = useState(false);
+    const [transferred, setTransferred] = useState(0);
+
+    const [filenameKTP, setFileNameKTP] = useState(null);
+    const [filenamePicture, setFileNamePicture] = useState(null);
+
+
+    const [text, setText] = useState({
+        profile_pic: readyUploadPhoto,
+
+        profiles: {
+
+            KTP_KTM: readyUploadKTP,
+
+        }
+    })
+
+    // const [imagetag, setImagetag] = useState({
+    //     profile_pic: imagePicker2,
+    //     profiles: {
+    //         KTP_KTM: imagePicker,
+    //     }
+    // })
+
+    const option = {
+        title: 'Pilih KTP',
+        type: 'library',
+        option: {
+            selectionLimit: 1,
+            mediaType: 'photo',
+        }
+    }
+
+    const openGalery = async () => {
+        const imagesKTP = await launchImageLibrary(option);
+        if (imagesKTP.didCancel) {
+            console.log('cancel Image')
+        } else if (imagesKTP.errorCode) {
+            console.log(imagesKTP.errorMessage)
+        } else {
+            const source = { uri: imagesKTP.assets[0].uri }
+            setImagePicker(source)
+            console.log(source)
+            console.log(imagesKTP.assets[0].fileName)
+        }
+
+    }
+
+    const openGalery2 = async () => {
+        const imagesPic = await launchImageLibrary(option);
+        if (imagesPic.didCancel) {
+            console.log('cancel Image')
+        } else if (imagesPic.errorCode) {
+            console.log(imagesPic.errorMessage)
+        } else {
+            const source = { uri: imagesPic.assets[0].uri }
+            setImagePicker2(source)
+            console.log(source)
+            console.log(imagesPic.assets[0].fileName)
+        }
+
+
+    }
+
+    const uploadPhoto = async () => {
+        const { uri } = imagePicker2
+        const fileName = uri.substring(uri.lastIndexOf('/') + 1);
+        //setFileNamePicture(fileName)
+        console.log(fileName)
+        const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+
+        setTransferred(0);
+
+        const task = storage().ref('/profile_picture/' + fileName).putFile(uploadUri)
+
+        task.on('state_changed', snapshot => {
+            setTransferred(
+                Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000
+            );
+        });
+
+        try {
+            await task
+        } catch (e) {
+            console.error(e);
+        }
+
+        let imageProfileRef = await firebase.storage().ref('/profile_picture/' + fileName)
+        await imageProfileRef.getDownloadURL().then((url) => {
+            setText(text => ({
+                ...text, profile_pic: url
+            }));
+            //setUploadPhoto(url)
+        }).catch((e) => console.log('getting downloadURL of image error => ', e))
+
+
+    }
+
+    const uploadKTP = async () => {
+        const { uri } = imagePicker
+        const fileName = uri.substring(uri.lastIndexOf('/') + 1);
+        //setFileNameKTP(fileName)
+        console.log(fileName)
+        const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+
+        setTransferred(0);
+
+        const task = storage().ref('/ktp_ktm/' + fileName).putFile(uploadUri)
+
+        task.on('state_changed', snapshot => {
+            setTransferred(
+                Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000
+            );
+        });
+
+        try {
+            await task
+        } catch (e) {
+            console.error(e);
+        }
+
+        let imageKTPRef = await firebase.storage().ref('/ktp_ktm/' + fileName)
+        await imageKTPRef.getDownloadURL().then((url) => {
+            setText(text => ({
+                ...text,
+                profiles: {
+                    ...text.profiles, KTP_KTM: url
+                }
+            }));
+            //setUploadKTP(url)
+        }).catch((e) => console.log('getting downloadURL of image error => ', e))
+
+
+    }
+
+    // const handleFilePic = () => {
+    //     let imageProfileRef = firebase.storage().ref('/profile_picture/' + filenamePicture)
+    //     imageProfileRef.getDownloadURL().then((url) => {
+    //         setUploadPhoto(url)
+    //     }).catch((e) => console.log('getting downloadURL of image error => ', e))
+
+    //     let imageKTPRef = firebase.storage().ref('/ktp_ktm/' + filenameKTP)
+    //     imageKTPRef.getDownloadURL().then((url) => {
+    //         setUploadKTP(url)
+    //     }).catch((e) => console.log('getting downloadURL of image error => ', e))
+
+
+    //     if (readyUploadKTP != null && readyUploadPhoto != null) {
+    //         console.log(readyUploadKTP)
+    //         setText(text => ({
+    //             ...text,
+    //             profiles: {
+    //                 ...text.profiles, KTP_KTM: readyUploadKTP
+    //             }
+    //         }));
+    //         console.log(text)
+    //         console.log(readyUploadPhoto)
+    //         setText(text => ({
+    //             ...text, profile_pic: readyUploadPhoto
+    //         }));
+    //     }
+    // }
+
+    const insertData = () => {
+
+        uploadKTP()
+        uploadPhoto()
+        // if (filenameKTP != null && filenamePicture != null) {
+        //     console.log('masuk handle flie')
+        //     handleFilePic()
+        // }
+        setTimeout(() => {
+            console.log('masuk handle masukkkkk database')
+            let ipMasuk = ('http://192.168.43.140:8000/sirius_api/register_user/40/').toLowerCase()
+            //let ipMasuk = ('https://sirius-oail.loca.lt/sirius_api/register_user/10/')
+            console.log(ipMasuk)
+            return fetch(
+                ipMasuk,
+                {
+                    method: 'patch',
+                    body: JSON.stringify(text),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+
+            ).then(response => response.json())
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
+        }, 5000);
+        // if (readyUploadKTP != null && readyUploadPhoto != null) {
+        //     console.log('masuk handle masukkkkk database')
+        //     let ipMasuk = ('http://192.168.43.140:8000/sirius_api/register_user/40/').toLowerCase()
+        //     //let ipMasuk = ('https://sirius-oail.loca.lt/sirius_api/register_user/10/')
+        //     console.log(ipMasuk)
+        //     return fetch(
+        //         ipMasuk,
+        //         {
+        //             method: 'patch',
+        //             body: JSON.stringify(text),
+        //             headers: {
+        //                 'Content-Type': 'application/json',
+        //             },
+        //         }
+
+        //     ).then(response => response.json())
+        //         .then(response => console.log(response))
+        //         .catch(error => console.log(error))
+        // }
+    }
+
+
+
+
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.logo}>
+                    <Image
+                        source={require('../assets/images/logo.png')}
+                        style={styles.logoImage}
+                    />
+                </View>
+                <View style={styles.input}>
+
+                    <View style={styles.inputArea1}>
+                        <TouchableOpacity style={styles.touch} onPress={openGalery}>
+                            <Text style={styles.textPic}>Pilih</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.textdest}>{imagePicker != null ? imagePicker['uri'].substring(imagePicker['uri'].lastIndexOf('/') + 1) : 'KTP/KTM'}</Text>
+                    </View>
+
+                    <View style={styles.inputArea1} >
+                        <TouchableOpacity style={styles.touch} onPress={openGalery2}>
+                            <Text style={styles.textPic}>Pilih</Text>
+
+                        </TouchableOpacity>
+                        <Text style={styles.textdest}>{imagePicker2 != null ? imagePicker2['uri'].substring(imagePicker2['uri'].lastIndexOf('/') + 1) : 'Pas Foto'}</Text>
+                    </View>
+
+                </View>
+
+                <TouchableOpacity
+                    style={styles.masukButton}
+                    //onPress={insertData}
+                    onPress={insertData}
+                >
+                    <Text style={styles.buttonText1}>Daftar</Text>
+                </TouchableOpacity>
+
+                <View style={styles.forgetPass}>
+                    <Text style={styles.forget}>Sudah Memiliki Akun ?</Text>
+                    <TouchableOpacity
+                        style={styles.reset}
+
+                    >
+                        <Text style={styles.reset}>Masuk</Text>
+                    </TouchableOpacity>
+                    <View style={{ height: 40 }} />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+export default RegisterAfter
--- User/src/screen/RegisterPage.js
+++ User/src/screen/RegisterPage.js
@@ -1,80 +1,184 @@
-import React from 'react';
-import { 
-  Text, 
-  SafeAreaView, 
+import React, { useState } from 'react';
+import {
+  Text,
+  SafeAreaView,
   ScrollView,
-  View, 
-  TextInput, 
-  TouchableOpacity } from 'react-native';
+  View,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 import styles from '../style/LoginPageStyles';
-import { Image,  } from 'react-native-elements';
-const Login = ({ navigation }) => {
+import { Image, } from 'react-native-elements';
+
+import { Picker } from "@react-native-picker/picker";
+import { useNavigation } from '@react-navigation/native';
+
+const Register = () => {
+
+  const navigation = useNavigation();
+
+  const [id, setId] = useState('');
+  const [userData, setUserData] = useState(null)
+
+  const [akhir, setAkhir] = useState('mahasiswa');
+  const [text, setText] = useState({
+    email: '',
+    nama: '',
+    password: '',
+    role: akhir,
+    profiles: {
+      prodi_unit_institusi: '',
+      alamat: '',
+      NRK_NIK_NIP_NIM: '',
+      status_verifikasi: '0',
+    }
+
+
+  })
+  const handleTextChange = (textChange) => {
+    return (val) => {
+      setText({ ...text, [textChange]: val });
+    }
+  }
+  const handleTextProfiles = (textChange) => {
+    return (val) => {
+      setText({
+        ...text,
+        profiles: {
+          ...text.profiles,
+          [textChange]: val,
+        },
+      })
+    }
+  }
+
+
+
+
+
+
+  const insertData = () => {
+
+    return fetch(
+      'http://192.168.43.140:8000/sirius_api/register_user/',
+      {
+        method: 'post',
+        body: JSON.stringify(text),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+
+    ).then(response => response.json())
+      // .then(response => console.log(response))
+      .then((json) => setUserData(json))
+      .catch(error => console.log(error))
+  }
+
+  const toResgisterAfter = () => {
+
+    insertData()
+
+
+
+    if (userData != null && userData.user != null) {
+
+      console.log(userData.user.id)
+
+      navigation.navigate("RegisterAfter", {
+        id: userData.user.id
+      })
+    }
+
+  }
+
+  // console.log(text)
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <View style={styles.logo}>
           <Image
-            source={ require('../assets/logo.png')}
+            source={require('../assets/images/logo.png')}
             style={styles.logoImage}
           />
         </View>
         <View style={styles.input}>
-          <TextInput 
+          <TextInput
             placeholder='Nama Lengkap'
             style={styles.inputArea}
+            onChangeText={handleTextChange('nama')}
+            value={text.nama}
           />
           <TextInput
-            placeholder='Input'
+            placeholder='Email'
             style={styles.inputArea}
+            onChangeText={handleTextChange('email')}
+            value={text.email}
           />
-          <TextInput 
+          <TextInput
             placeholder='Password'
             style={styles.inputArea}
+            onChangeText={handleTextChange('password')}
+            value={text.password}
           />
-          <TextInput 
+          <TextInput
             placeholder='Konfirmasi Password'
             style={styles.inputArea}
           />
-          <TextInput 
-            placeholder='Peran'
-            style={styles.inputArea}
-          />
-          <TextInput 
+          <View style={styles.inputArea1}>
+            <Picker
+              selectedValue={akhir}
+              onValueChange={(value, index) => setAkhir(value)}
+              mode="dropdown" // Android only
+              style={styles.picker}
+              itemStyle={{ alignItems: 'center', justifyContent: 'center' }}
+
+            >
+
+              <Picker.Item label="Mahasiswa" value="mahasiswa" />
+              <Picker.Item label="Dosen" value="dosen" />
+              <Picker.Item label="Umum" value="masyarakat" />
+            </Picker>
+          </View>
+
+          <TextInput
             placeholder='Instansi/Unit/Program Studi'
             style={styles.inputArea}
+            onChangeText={handleTextProfiles('prodi_unit_institusi')}
+            value={text.instansi}
           />
-          <TextInput 
+          <TextInput
             placeholder='NIK/NRK/NIP/NIM'
             style={styles.inputArea}
+            onChangeText={handleTextProfiles('NRK_NIK_NIP_NIM')}
+            value={text.nik}
           />
-          <TextInput 
+          <TextInput
             placeholder='Alamat'
             style={styles.inputArea}
+            onChangeText={handleTextProfiles('alamat')}
+            value={text.alamat}
           />
-          <TextInput 
-            placeholder='KTP/KSM'
-            style={styles.inputArea}
-          />
-          <TextInput 
-            placeholder='Pas Foto'
-            style={styles.inputArea}
-          />
+
         </View>
-        <View style={styles.masukButton}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={navigation.navigate('Register')}
-          >
-            <Text style={styles.buttonText}>Register</Text>
-          </TouchableOpacity>       
-        </View>
+
+        <TouchableOpacity
+          style={styles.masukButton}
+          onPress={toResgisterAfter}
+        >
+          <Text style={styles.buttonText1}>Selanjutnya</Text>
+        </TouchableOpacity>
+
         <View style={styles.forgetPass}>
           <Text style={styles.forget}>Sudah Memiliki Akun ?</Text>
-          <TouchableOpacity 
-          style={styles.reset}
-          onPress={navigation.navigate('Login')}>
+          <TouchableOpacity
+            style={styles.reset}
+
+          >
             <Text style={styles.reset}>Masuk</Text>
-            </TouchableOpacity>
+          </TouchableOpacity>
+          <View style={{ height: 40 }} />
         </View>
       </ScrollView>
     </SafeAreaView>
