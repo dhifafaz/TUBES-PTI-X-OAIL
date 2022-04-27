083c741123925bdcd4f579f4b3ebd9bf17c3dc4b
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-04-27 13:21:27 +0700

    gak tau ini apa

--- backendsirius/backendsirius/settings.py
+++ backendsirius/backendsirius/settings.py
@@ -44,7 +44,7 @@ CORS_ALLOW_CREDENTIALS = False
 REST_KNOX = {
   'SECURE_HASH_ALGORITHM': 'cryptography.hazmat.primitives.hashes.SHA512',
   'AUTH_TOKEN_CHARACTER_LENGTH': 64,
-  'TOKEN_TTL': None,
+  'TOKEN_TTL': timedelta(hours=8766),
   'USER_SERIALIZER': 'knox.serializers.UserSerializer',
   'TOKEN_LIMIT_PER_USER': None,
   'AUTO_REFRESH': False,
