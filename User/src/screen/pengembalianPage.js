0c4b7d5773e450210bba297e71bc644a7814cf49
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-05-08 18:14:59 +0700

    user selesai

--- User/src/screen/pengembalianPage.js
+++ User/src/screen/pengembalianPage.js
@@ -16,6 +16,7 @@ import { getDataOrderLog, getPinjamAlat } from '../redux/action';
 import { useSelector, useDispatch } from 'react-redux';
 
 let status = []
+let idUser = []
 
 const PengembalianPage = ({ navigation }) => {
 
@@ -31,9 +32,26 @@ const PengembalianPage = ({ navigation }) => {
 
     const inputToAPI = async () => {
         for (let i = 0; i < status.length; i++) {
-
+            if (status[i] == 'digunakan') {
+                let ubah = {
+                    status_order: 'meminta-pengembalian'
+                }
+                await fetch(
+                    'http://192.168.43.140:8000/sirius_api/order_log/' + idUser[i] + '/',
+                    {
+                        method: 'patch',
+                        body: JSON.stringify(ubah),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                ).then(response => response.json())
+                    .then(response => console.log(response))
+                    .catch(error => console.log(error))
+            }
         }
     }
+    console.log(check)
 
     return (
         <SafeAreaView style={styles.color}>
@@ -47,7 +65,7 @@ const PengembalianPage = ({ navigation }) => {
                     {
                         check == 1 ?
                             <View style={styles.ambilView}>
-                                <TouchableOpacity style={styles.ambilTouch}>
+                                <TouchableOpacity style={styles.ambilTouch} onPress={() => inputToAPI()}>
                                     <Text style={styles.ambilText}>Kembalikan</Text>
                                 </TouchableOpacity>
                             </View> : null
@@ -59,7 +77,9 @@ const PengembalianPage = ({ navigation }) => {
                         data={dataOrderLog}
                         renderItem={({ item, index, separators }) => {
                             if (item.status_order === 'digunakan') {
-                                status[index] = item.id
+                                status[index] = item.status_order
+                                idUser[index] = item.id
+                                console.log('test')
                                 setCheck(1)
                                 return (
                                     <View>
