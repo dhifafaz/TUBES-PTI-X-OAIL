0d178e0e57e0a1fd1347954039aa6152963d5df2
elizamaharani <elizamaharani@gmail.com>
2022-05-16 12:01:01 +0700

    fitur search daftar peminjam

--- Petugas/src/screen/peminjam.js
+++ Petugas/src/screen/peminjam.js
@@ -26,6 +26,53 @@ const wait = (timeout) => {
 
 const PeminjamanPage = ({ navigation }) => {
 
+    const [search, setSearch] = useState('');
+    const [filteredDataSource, setFilteredDataSource] = useState([]);
+    const [masterDataSource, setMasterDataSource] = useState([]);
+
+    useEffect(() => {
+        
+        fetch('http://192.168.42.104:8000/sirius_api/daftar_peminjam/')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson.data_peminjam);
+            setFilteredDataSource(responseJson.data_peminjam);
+            setMasterDataSource(responseJson.data_peminjam);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }, []);
+
+    const searchFilterFunction = (text) => {
+        // Check if searched text is not blank
+        if (text) {
+        // Inserted text is not blank
+        // Filter the masterDataSource
+        // Update FilteredDataSource
+        const newData = masterDataSource.filter(function (item) {
+            const itemData = item.nama_user
+            ? item.nama_user.toUpperCase()
+            : ''.toUpperCase();
+            const textData = text.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+            
+        });
+        setFilteredDataSource(newData);
+        setSearch(text);
+        } else {
+        // Inserted text is blank
+        // Update FilteredDataSource with masterDataSource
+        setFilteredDataSource(masterDataSource);
+        setSearch(text);
+        }
+    };
+
+    const getItem = (item) => {
+        // Function for click on an item
+        alert('Id : ' + item.id_user + ' Title : ' + item.nama_user);
+    };
+
     const [refreshing, setRefreshing] = React.useState(false);
 
     const onRefresh = React.useCallback(() => {
@@ -63,12 +110,36 @@ const PeminjamanPage = ({ navigation }) => {
                     <ProfilBar />
                     <View style={styles.enter30} />
 
-                    <SearchingBar />
+                    <SearchBar
+                        round
+                        searchIcon={{ size: 24 }}
+                        onChangeText={(text) => searchFilterFunction(text)}
+                        onClear={(text) => searchFilterFunction('')}
+                        placeholder="Type Here..."
+                        value={search}
+                        lightTheme
+                        containerStyle={{
+                            width: "100%",
+                            borderColor: 'none',
+                            backgroundColor: '#ECECEC',
+                            border: 'none',
+                            height: 50,
+                            borderRadius: 10,
+                        }}
+                        inputContainerStyle={{
+                            height: 0,
+                            backgroundColor: '#ECECEC',
+                            borderRadius: 10,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            fontFamily: 'Ubuntu-Medium',
+                        }}
+                     />
                     <View style={styles.enter30} />
 
 
                     <FlatList
-                        data={daftarPeminjman["data_peminjam"]}
+                        data={filteredDataSource}
                         renderItem={({ item, index, separators }) => {
 
                             if (item.status_order === 'proses' || item.status_order === 'meminta-pengambilan' || item.status_order === 'meminta-pengembalian' || item.status_order === 'digunakan') {
